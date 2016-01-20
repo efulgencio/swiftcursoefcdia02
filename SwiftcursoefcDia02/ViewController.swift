feaaf8dd938d3f761eb282c8937736d4312c9d45
eduardo fulgencio <eduardofulgencio@macbook-pro-de-eduardo.local>
2016-01-20 16:25:44 +0100

    Initial Commit

---
+++ SwiftcursoefcDia02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftcursoefcDia02
+//
+//  Created by eduardo fulgencio on 20/1/16.
+//  Copyright © 2016 eduardo fulgencio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
